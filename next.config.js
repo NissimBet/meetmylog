b6acfd641cd7e85eba890279631004f895c12e3b
NissimBet <nissimbet123@outlook.com>
2020-05-12 20:33:32 -0500

    Agregado dotenv para variabels de ambiente

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
   webpack: config => {
     config.node = {
       fs: 'empty',
+      BACKEND_URI: process.env.BACKEND_URI,
     };
     return config;
   },
